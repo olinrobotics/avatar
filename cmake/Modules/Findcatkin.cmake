2a42741d682eab359ceea29847ee3c058e99b2e0
snielsen221b <sophianielsen221b@gmail.com>
2017-07-21 10:15:06 -0400

    Finished Findcatkin.cmake, tested, error: could not find std_msgs

--- cmake/Modules/Findcatkin.cmake
+++ cmake/Modules/Findcatkin.cmake
@@ -6,4 +6,20 @@ SET(CATKIN_LibrarySearchPaths
         /usr/local/lib
         /usr/lib)
 
-FIND_LIBRARY()
+FIND_LIBRARY(CATKIN_LIBRARY
+        NAMES catkin
+        PATHS ${DLIB_LibrarySearchPaths}
+        CMAKE_PREFIX_PATH std_msgs)
+
+FIND_PACKAGE_AHNDLE_STANDARD_ARGS(CATKIN "Could NOT find CATKIN library"
+        CATKIN_LIBRARY
+        CATKIN_INCLUDE_DIR)
+
+SET(CATKIN_LIBRARIES)
+LIST(APPEND CATKIN_LIBRARIES ${CATKIN_LIBRARY})
+
+MARK_AS_ADVANCED(
+        CATKIN_INCLUDE_DIR
+        DLIB_LIBRARIES
+        DLIB_FOUND
+)
