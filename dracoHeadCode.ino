60837f803519a498fc3c8294ff85d3307cd76e01
joannakennedy <kennedy.js1@gmail.com>
2017-07-31 10:07:12 -0400

    Add files via upload
    melody tested and works :)

--- dracoHeadCode.ino
+++ dracoHeadCode.ino
@@ -1,15 +1,4 @@
 #include <Servo.h>//include built in Arduino servo library
-//     note, period 
-#define c  3830  
-#define d  3400  
-#define e  3038  
-#define f  2864 
-#define g  2550  
-#define a  2272  
-#define b  2028 
-#define C  1912
-// Define a special note, 'R', to represent a rest
-#define R 0
 
 // Set up Arduino Ports and Pins to support Robot
 int redLedPin = 11;// robot alive red blinky light pin
@@ -20,7 +9,10 @@ int eStopButtonState= 0; // eStop button status
 int delayPeriod = 100; // hindbrain loop delay 
 int DT = 0; // create name for sharp ir 0 analog input pin 0 / distance sensor for outside objects / sharpDistance0--> DT
 int DM = 1; // create name for sharp ir 1 analog input pin 1-- distance sensor for anything in mouth / sharpDistance1--> DM
-int speakerOut = 6;// create name for speaker pin
+int speakerPin = 6;// create name for speaker pin
+int soundPin = 6;// create name for sound pin
+int numtones = 7;         // create number of tones
+int tones[] = { 392, 379, 349, 330, 349, 370, 392 };  //stored tones
 int jaw = 60;  // variable to store jaw servo position in degrees
 Servo jawServo; // create jaw servo object to control a servo
 char command = 'g' ;// 'g' is go command from midbrain 's' is stop,
@@ -54,53 +46,14 @@ pinMode (eStopPin, INPUT);  //sets up Sense input of E-Stop button
 jawServo.write(60);//set initial jaw position to 60 degrees or open
 jawServo.attach(3);//attach the jaw servo to pin 3
 Serial.begin(9600);//send and recieve at 9600 baud
- pinMode(speakerOut, OUTPUT);
+pinMode(speakerPin, OUTPUT);
  // Run Hindbrain loop until commanded to stop by Midbrain
-pinMode(speakerOut, OUTPUT);
-int melody[]= {c,d,e,f,g,R,C};// melody
-int beats[]= { 16,16,16,16,16,32,8 }; // note length, higher number means longer note
-int MAX_COUNT = sizeof(melody) / 2; // Melody length, for looping
+  for (int i=0; i < numtones; i++)
+  { tone(speakerPin, tones[i]);
+    delay(200);
+  }
+  noTone(speakerPin);
 }
-long tempo = 10000; // Set length of pause between notes
-int pause = 1000; // Loop variable to increase Rest length
-int rest_count = 100; //rest
-int tone_= 0; // Initialize core variables
-int beat= 0;
-long duration= 0;
-void playTone() {
- long elapsed_time = 0;
- if (tone_ > 0) { // if this isn't a Rest beat, while the tone has 
-//played less long than 'duration', pulse speaker HIGH and LOW
-while (elapsed_time < duration) {
-digitalWrite(speakerOut,HIGH);
-delayMicroseconds(tone_ / 2);
-digitalWrite(speakerOut, LOW);
-delayMicroseconds(tone_ / 2);
-elapsed_time += (tone_);
- }
-}
-else {
-for (int j = 0; j < rest_count; j++) { 
-delayMicroseconds(duration);
-} 
- }
-}
-void melody (){
-  int MAX_COUNT = sizeof(melody) / 2;
-  int beats[]= { 16,16,16,16,16,32,8 };
-  int beat= 0;
-for (int i=0; i<MAX_COUNT; i++) {
-tone_ = melody[i];
-beat = beats[i];
-
-duration = beat * tempo; // Set up timing
-
-playTone(); 
-// A pause between notes...
-delayMicroseconds(pause);
-}
-}
-
 void loop() {
  // put your main code here, to run repeatedly:
  // Hindbrain functions 
@@ -148,8 +101,10 @@ if (eStopButtonState == HIGH){
  hBrainStatus = 's' ; // if E-Stop switch triggered set Hindbrain status to E-Stopped
 }
 if (hBrainStatus = 'r'){ // else set status to running
-Serial.println (sharpRange (DT)); //Print range from Sharp on Pin A0 (debug only)
-Serial.println (sharpRange (DM)); //Print range from Sharp on Pin A1 (debug only)
+  Serial.println ("Nose distance:");
+Serial.println (sharpRange (DT)); //Print range from Sharp on Pin A0
+Serial.println ("Mouth distance:");
+Serial.println (sharpRange (DM)); //Print range from Sharp on Pin A1 
   float DT= sharpRange(DT);  // Read Sharp mouth range 0 on Pin A0-- distance sensor for outside objects
   float DM= sharpRange(DM);  // Read Sharp nose range 1 on Pin A1-- distance sensor for anything in mouth
 }
