14bae26261e0ec7b95b248fd5f4b48969df7345d
joannakennedy <kennedy.js1@gmail.com>
2017-08-07 10:24:15 -0400

    Add files via upload
    working on picking up

--- dracoHeadCode.ino
+++ dracoHeadCode.ino
@@ -10,11 +10,11 @@ int delayPeriod = 100; // hindbrain loop delay
 int DT = 0; // create name for sharp ir 0 analog input pin 0 / distance sensor for outside objects / sharpDistance0--> DT
 int DM = 1; // create name for sharp ir 1 analog input pin 1-- distance sensor for anything in mouth / sharpDistance1--> DM
 int speakerPin = 6;// create name for speaker pin
-int soundPin = 6;// create name for sound pin
 int numtones = 7;         // create number of tones
 int tones[] = { 392, 379, 349, 330, 349, 370, 392 };  //stored tones
-int jaw = 60;  // variable to store jaw servo position in degrees
+int jaw = 55;  // variable to store jaw servo position in degrees
 Servo jawServo; // create jaw servo object to control a servo
+int jawAngle = 55;
 char command = 'g' ;// 'g' is go command from midbrain 's' is stop,
 char hBrainStatus = 'r'; // hindbrain status 'r' running, 's' E-Stopped
 char pickUp= 'p' ; //'p' pickup command
@@ -57,120 +57,116 @@ pinMode(speakerPin, OUTPUT);
 void loop() {
  // put your main code here, to run repeatedly:
  // Hindbrain functions 
- // Sense Functions
 
 
+  // Read Midbrain commands
+  command = 'g';
 
-// Think Functions
-
-
-
-// Act Functions
-
-// Read Midbrain commands
- while (Serial.available()) {// checks to see if you have message in serial buffer
- command = Serial.read();// read character command from serial monitor
- readString += command; //makes the string readString to store commands
- delay(5000);   //slow loop to allow buffer to fill with next character
- }
-
-if (Serial.available ()) {
- command = Serial.read(); //read character command from serial monitor
- Serial.println ("Mindbrain sent:");
- Serial.println (command); //print midbrain command in serial monitor
-}
-
+  while (Serial.available()) {// checks to see if you have message in serial buffer
+    command = Serial.read();// read character command from serial monitor
+    readString += command; //makes the string readString to store commands
+    delay(500);   //slow loop to allow buffer to fill with next character
+    Serial.println ("Mindbrain sent:");
+    //Serial.println (command); //print midbrain command in serial monitor
+  }
+  
  if (readString.length() >0) {
- Serial.println(readString);  //so you can see the captured string 
+   Serial.println(readString);  //so you can see the captured string 
  if (command != 's') {//during stop, ignore command to move jaw
- jaw = readString.toInt();  //convert readString into a jaw angle
- } 
+  jaw = readString.toInt();  //convert readString into a jaw angle
+ }
  readString=""; //empty readString for next input
- } 
+ }
 
 eStopButtonState= digitalRead(eStopPin);
  
-// S
-// E
-// N
-// S
-// E
+//-------------------------------------------------------------------------------------------
+// SENSE
+
 // Read and Process Robot Sensors
 //Serial.println (readEstop()); //read and print
 if (eStopButtonState == HIGH){
  hBrainStatus = 's' ; // if E-Stop switch triggered set Hindbrain status to E-Stopped
 }
-if (hBrainStatus = 'r'){ // else set status to running
-  Serial.println ("Nose distance:");
-Serial.println (sharpRange (DT)); //Print range from Sharp on Pin A0
-Serial.println ("Mouth distance:");
-Serial.println (sharpRange (DM)); //Print range from Sharp on Pin A1 
-  float DT= sharpRange(DT);  // Read Sharp mouth range 0 on Pin A0-- distance sensor for outside objects
-  float DM= sharpRange(DM);  // Read Sharp nose range 1 on Pin A1-- distance sensor for anything in mouth
-}
 
-//T
-//H
-//I
-//N
-//K
+// --------------------------------------------------------------------------------------------------------
+// THINK
+
 //Run low level cognition and safety code
 if (command == 'g') { 
-digitalWrite (blueLedPin, HIGH);
-delay (delayPeriod);
-digitalWrite (blueLedPin, LOW);
-delay(delayPeriod);
-digitalWrite (greenLedPin, HIGH);
-delay (delayPeriod);
-digitalWrite (greenLedPin, LOW);
-delay(delayPeriod);
-digitalWrite (redLedPin, HIGH);
-delay (delayPeriod);
-digitalWrite (redLedPin, LOW);
-delay (delayPeriod);  // blink hindbrain running LEDs
+  digitalWrite (blueLedPin, HIGH);
+  delay (delayPeriod);
+  digitalWrite (blueLedPin, LOW);
+  delay(delayPeriod);
+  digitalWrite (greenLedPin, HIGH);
+  delay (delayPeriod);
+  digitalWrite (greenLedPin, LOW);
+  delay(delayPeriod);
+  digitalWrite (redLedPin, HIGH);
+  delay (delayPeriod);
+  digitalWrite (redLedPin, LOW);
+  delay (delayPeriod);  // blink hindbrain running LEDs 
+  Serial.println("Hind brain running!!!!");  //print hindbrain status on serial monitor
+  Serial.println ("Commanding jaw angle:");
+  float DT= sharpRange(DT);  // Read Sharp mouth range 0 on Pin A0-- distance sensor for outside objects
+  float DM= sharpRange(DM);  // Read Sharp nose range 1 on Pin A1-- distance sensor for anything in mouth
+  Serial.println ("Distance to outside objects (DT):");
+  Serial.println (sharpRange (DT)); //Print range from Sharp on Pin A0
+  Serial.println ("Distance in mouth (DM):");
+  Serial.println (sharpRange (DM)); //Print range from Sharp on Pin A1 
+  //Serial.println ("Jaw angle:");
+  //Serial.println (jaw);
+  //jawServo.write (jaw);
+  delay (1000);// chill 
 }
-else { //if e-Stopepd
-delay (delayPeriod);
-delay (delayPeriod); 
+else if (command == 'p') { //else if :if dm<20 println chomp else jawServo.write (+1)
+  Serial.println ("Pick up");
+  jawServo.write(55); // command open mouth
+  delay(1000);
+  if (sharpRange (DM) < 33){   // if something in the mouth
+    if (sharpRange (DM) < 10){
+      Serial.println ("Chomp");
+    }
+    else {
+    jawAngle= jawAngle+5; // command to slowly close down on object
+    jawServo.write(jawAngle); 
+    delay(1);
+    }
+    command = 'g'; 
+  }
 }
-if (command == 'p') {
-Serial.println ("Pick up");
-jawServo.write(60); // command open mouth
-delay(500);
-if (DM < 20.0){   // if something in the mouth
-jawServo.write(70);  // command close mouth
-delay(500);
-
-}
-if (command == 'd') {
+else if (command == 'd') {
   Serial.println ("Drop");
-  jawServo.write(60);  // command open mouth
+  jawServo.write(55);  // command open mouth
   delay(500);
+  command = 'g';
 }
-if (DT < 15.0){
+else if (command == 'o') {
+  Serial.println ("Open");
+  jawServo.write(55);  // command open mouth
+  delay(500);
+  command = 'g';
+}
+else if (command == 'c') {
+  Serial.println ("Close");
+  jawServo.write(70);  // command open mouth
+  delay(500);
+  command = 'g';
+}
+if (sharpRange (DT)< 30.0){
   hBrainStatus = 's' ;
 }
-//A
-//C
-//T
+//-------------------------------------------------------------------------------
+//ACT
+
 //Run actuators and behavior lights
 
 // Write status data up to MidBrain
 if (command == 's') {
-Serial.println("hind brain stopped");//print hindbrain status on serial monitor
+  Serial.println("hind brain stopped");//print hindbrain status on serial monitor
 }
-if (command == 'g') {
-Serial.println("Hind brain running!!!!");  //print hindbrain status on serial monitor
-Serial.println ("Commanding jaw angle:");
-Serial.println ("Distance to outside objects (DT):");
-Serial.println (DT);//distance sensor for outside objects
-Serial.println ("Distance in mouth (DM):");
-Serial.println (DM);//distance sensor for anything in mouth
-Serial.println ("Jaw angle:");
-Serial.println (jaw);
-jawServo.write (jaw);
-} 
+ 
 }
-}
+
 
 
